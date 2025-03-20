0b3886a6c07df72d8da9d273c9a7eddbbbaf424d
Pradip Sahu <sahuprasantakumar975@gmail.com>
2025-03-20 18:52:10 +0530

    Initial commit

--- loger.go
+++ loger.go
@@ -2,10 +2,19 @@ package mylogger
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
+	"net"
 	"os"
 	"time"
 
 	"github.com/sirupsen/logrus"
+)
+
+// Graylog configuration
+const (
+	GraylogHost = "34.93.173.35" // Change to your actual Graylog server IP
+	GraylogPort = "12201"        // Default GELF port
 )
 
 // LogData represents the structured log format
@@ -29,7 +38,7 @@ type LogData struct {
 	ParamC       string `json:"param_c,omitempty"`
 }
 
-// Logger wraps logrus
+// Logger struct
 type Logger struct {
 	logger *logrus.Logger
 }
@@ -38,16 +47,19 @@ type Logger struct {
 func NewLogger() *Logger {
 	l := logrus.New()
 	l.SetFormatter(&logrus.JSONFormatter{})
-	l.SetOutput(os.Stdout) // Can be changed to a file
+	l.SetOutput(os.Stdout) // Also log to the console
+
 	return &Logger{logger: l}
 }
 
-// Log method to log messages
+// Log logs a message and sends it to Graylog
 func (l *Logger) Log(level, message string, data LogData) {
 	data.Timestamp = time.Now().UTC().Format(time.RFC3339)
-	data.Message = maskSensitiveData(message) // Mask if needed
+	data.Message = maskSensitiveData(message) // Mask sensitive data if needed
 
 	jsonData, _ := json.Marshal(data)
+	
+	// Log locally
 	switch level {
 	case "INFO":
 		l.logger.Info(string(jsonData))
@@ -58,13 +70,57 @@ func (l *Logger) Log(level, message string, data LogData) {
 	default:
 		l.logger.Warn(string(jsonData))
 	}
+
+	// Send to Graylog
+	go sendToGraylog(jsonData) // Run in a goroutine to avoid blocking
 }
 
 // Mask sensitive data
 func maskSensitiveData(message string) string {
-	// Example: mask everything except first and last character
 	if len(message) > 2 {
 		return string(message[0]) + "****" + string(message[len(message)-1])
 	}
 	return "****"
+}
+
+// sendToGraylog sends log data to Graylog over both UDP and TCP
+func sendToGraylog(logData []byte) {
+	udpAddr := fmt.Sprintf("%s:%s", GraylogHost, GraylogPort)
+	tcpAddr := fmt.Sprintf("%s:%s", GraylogHost, GraylogPort)
+
+	// Send over UDP
+	err := sendUDP(udpAddr, logData)
+	if err != nil {
+		log.Println("Failed to send log over UDP:", err)
+	}
+
+	// Send over TCP
+	err = sendTCP(tcpAddr, logData)
+	if err != nil {
+		log.Println("Failed to send log over TCP:", err)
+	}
+}
+
+// sendUDP sends log data over UDP
+func sendUDP(address string, data []byte) error {
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(data)
+	return err
+}
+
+// sendTCP sends log data over TCP
+func sendTCP(address string, data []byte) error {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(append(data, '\n')) // GELF messages should end with a newline
+	return err
 }
